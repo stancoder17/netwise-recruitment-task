70567a95514033f46aa352ed0780445a0a560580
stani <stanislawdyrda7@gmail.com>
2026-09-22 16:35:53 +0200

    Minor fix + a very important feature

--- netwise-recruitment-task/Controllers/NetwiseCatController.cs
+++ netwise-recruitment-task/Controllers/NetwiseCatController.cs
@@ -4,15 +4,18 @@ using netwise_recruitment_task.Services.Interfaces;
 
 namespace netwise_recruitment_task.Controllers;
 
+[ApiController]
 public class NetwiseCatController(ICatService service) : ControllerBase
 {
+    private readonly string _secretMessage = " /\\     /\\\n{  `---'  }\n{  O   O  }\n~~>  V  <~~\n \\  \\|/  /\n  `-----'____\n  /     \\    \\_\n {       }\\  )_\\_   _\n |  \\_/  |/ /  \\_\\_/ )\n  \\__/  /(_/     \\__/\n    (__/";
+    
     [HttpGet("/cat_fact")]
     public async Task<IActionResult> GetCatFact()
     {
         try
         {
             await service.FetchAndSaveAsync();
-            return Ok("Success!!! :-)");
+            return Ok("Success!!!\n\n\n" + _secretMessage);
         }
         catch (NotFoundException e)
         {
